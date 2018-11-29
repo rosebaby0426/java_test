de916940583481e2e9cd31c33985a0b61b9ef3f6
Java <Java@DESKTOP-TQ00GH9>
2018-11-29 21:45:32 +0800

    作業

--- src/Homework/HW7_1.java
+++ src/Homework/HW7_1.java
@@ -7,16 +7,29 @@ import java.io.IOException;
 public class HW7_1 {
 
 	public static void main(String[] args) {
-		int i, count = 0;
+		int i, count = 0, count1 = 0;
+		String str;
 		try {
+			// 因為檔案是中文字檔，所以用Reader來讀取
 			FileReader fr = new FileReader("C:\\CA106_Workspace\\HW\\Sample.txt");
 			BufferedReader br = new BufferedReader(fr);
 			while ((i = br.read()) != -1) {
 				System.out.print((char) i);
 				count++;
+				System.out.flush();
 			}
 			System.out.println();
 			System.out.println(count);
+//
+//			while ((str = br.readLine()) != null) {
+////				System.out.println(str);
+//				count1++;
+//
+//				System.out.println(count1);
+//				System.out.flush();
+//
+//			}
+
 			br.close();
 			fr.close();
 		} catch (IOException e) {
